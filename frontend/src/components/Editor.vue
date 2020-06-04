e77dd9e57c5fe166aca96e267c00a9c0c702f792
Miles Maddox <j.milesmaddox@gmail.com>
2020-06-04 00:09:00 -0500

    adding an initial tree view for files

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,24 +1,78 @@
 <template>
-  <div class="container">
-    <div class="centered">
-      <button @click="openFile" type="button">Open</button>
-    </div>
-
-    <div class="split right">
-      <textarea id="mde"></textarea>
-    </div>
-  </div>
+  <span>
+    <Split :style="windowHeight" :gutterSize="4">
+      <SplitArea :size="10">
+        <treeselect
+          v-model="value"
+          :options="files"
+          :searchable="true"
+          :clearable="false"
+          :multiple="false"
+          :default-expand-level="1"
+          :flatten-search-results="false"
+          placeholder="Select File..."
+        >
+          <label
+            slot="option-label"
+            slot-scope="{ node, shouldShowCount, count, labelClassName, countClassName }"
+            :class="labelClassName"
+          >
+            {{ node.isBranch ? '📂' : '📜' }}: {{ node.label }}
+            <span
+              v-if="shouldShowCount"
+              :class="countClassName"
+            >({{ count }})</span>
+          </label>
+        </treeselect>
+      </SplitArea>
+      <SplitArea :size="90">
+        <textarea id="mde"></textarea>
+      </SplitArea>
+    </Split>
+  </span>
 </template>
 
 <script>
 import EasyMDE from "easymde";
 // import * as Wails from '@wailsapp/runtime';
+// using ES6 modules
+// import VueSplit from "vue-split-panel";
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
 export default {
   data() {
     return {
-      defaultContent: ""
+      defaultContent: "",
+      windowHeight: "height: 800px;",
+      files: [
+        {
+          id: "a",
+          label: "http-signature",
+          children: [
+            {
+              id: "r",
+              label: "readme.md"
+            },
+            {
+              id: "ab",
+              label: "ab"
+            }
+          ]
+        },
+        {
+          id: "b",
+          label: "b"
+        },
+        {
+          id: "c",
+          label: "c"
+        }
+      ]
     };
+  },
+  components: {
+    Treeselect
   },
   methods: {
     openFile: function() {
@@ -89,7 +143,7 @@ export default {
         }
       });
 
-      easyMDE.toggleFullScreen();
+      // easyMDE.toggleFullScreen();
 
       // Send changes to Wails backend
       easyMDE.codemirror.on("change", function() {
@@ -101,6 +155,13 @@ export default {
         easyMDE.value(atob(result));
       });
     });
+  },
+  // Handle Window size
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  onResize() {
+    this.windowHeight = "height: " + window.innerHeight + "px;";
   }
 };
 </script>
@@ -113,41 +174,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* Split the screen in half */
-.split {
-  height: 100%;
-  width: 90%;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-/* Control the left side */
-.left {
-  left: 0;
-  background-color: #111;
-}
-
-/* Control the right side */
-.right {
-  right: 0;
-  background-color: red;
-}
-
-/* If you want the content centered horizontally and vertically */
-.centered {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
-
-/* Style the image inside the centered container, if needed */
-.centered img {
-  width: 150px;
-  border-radius: 50%;
+.labelClassName {
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import App from './App.vue';
+import VueSplit from 'vue-split-panel'
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+
+Vue.use(VueSplit)
 
 import * as Wails from '@wailsapp/runtime';
 
